81866d156f2fcce97c6cefc8f4287726eca91e54
martafrak <marta.frak94@gmail.com>
2018-11-25 13:27:36 +0100

    update test1

--- quality assurance/test1/auto_test1.py
+++ quality assurance/test1/auto_test1.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path=r'C:\TestFiles\chromedriver.exe')
 driver.get('http://martafrak.pl')
-
 title = driver.title
 print(title)
+assert 'My first website' == title
+driver.quit()
