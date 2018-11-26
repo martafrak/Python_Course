286201f36cac038c8fcb8a548e88aa2ee976cb12
martafrak <marta.frak94@gmail.com>
2018-11-26 19:35:01 +0100

    update auto_test2

--- quality assurance/test1/auto_test2.py
+++ quality assurance/test1/auto_test2.py
@@ -1,11 +1,25 @@
 import unittest
 from selenium import webdriver
 
+
 class FirstTest(unittest.TestCase): #class inherited from TestCase
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r'C:\TestFiles\chromedriver.exe')
+
     def test_main_page(self):
-        driver = webdriver.Chrome(executable_path=r'C:\TestFiles\chromedriver.exe')
+        driver = self.driver
         driver.get('http://martafrak.pl')
         title = driver.title
         print(title)
         assert 'My first website' == title
-        driver.quit()
+
+    def test_inspiration_page(self):
+        driver = self.driver
+        driver.get('http://martafrak.pl/inspiration')
+        title = driver.title
+        print(title)
+        assert 'My first website' == title
+
+    def tearDown(self):
+        self.driver.quit()
